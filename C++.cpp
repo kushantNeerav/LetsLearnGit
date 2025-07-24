d5243aa3e35fa5c3bf1f328740f869b564ec56ad
kushant.neerav04@gmail.com <kushant.neerav04@gmail.com>
2025-07-24 17:03:50 +0530

    Add 3 Files with different syntax

---
+++ C++.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "C++ is also one of the popular language in 2025";
+    return 0;
+}
---
+++ J.java
@@ -0,0 +1,7 @@
+
+public class J {
+
+    public static void main(String[] args) {
+        System.out.println("Java Is a popular language in 2025");
+    }
+}
